0fdef0fa164faf63b679f3aa9e9420cb4bfd818d
gaston964 <gastongutierrez964@gmail.com>
2022-10-26 18:49:42 -0300

    Arregle el boton eliminar

--- js/carrito.js
+++ js/carrito.js
@@ -7,7 +7,13 @@ let botonComprar = document.getElementById("boton-comprar");
 let botonVaciar = document.getElementById("boton-vaciar");
 let contadorCarrito = document.getElementById("contador-carrito");
 let precioTotal = document.getElementById("precio-total");
+let fragmento1 = document.createDocumentFragment();
+let fragmento2 = document.createDocumentFragment();
+let fragmento3 = document.createDocumentFragment();
+let fragmento4 = document.createDocumentFragment();
+let wapi = document.getElementById("wapi");
 let carrito = [];
+cargarEventos();
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("carrito")) {
         carrito = JSON.parse(localStorage.getItem("carrito"));
@@ -26,18 +32,17 @@ function cargarEventos() {
     contenedorRosados.addEventListener("click", agregarAlCarrito);
     contenedorEspumantes.addEventListener("click", agregarAlCarrito);
 };
-cargarEventos();
 function agregarAlCarrito(e) {
     if (e.target.classList.contains("agregar-al-carro")) {
         const prodSelec = e.target.parentElement.parentElement;
         dataProduc(prodSelec);
+        /* console.log(localStorage.getItem("carrito", JSON.stringify(carrito))); */
     }
 };
 function dataProduc(product) {
     const infoProd = {
-        imagen: product.querySelector("img").src,
         nombre: product.querySelector("h1").textContent,
-        precio: Number(product.querySelector(".price").innerText),
+        precio: Number(product.querySelector(".price").innerText.replace("$","")),
         id: product.querySelector("button").id,
         cantidad: 1,
         total: 0
@@ -72,19 +77,29 @@ const actualizarCarrito = () => {
         <p>$${precio}</p>
         <p>Cantidad: <span id="cantidad">${cantidad}</span></p>
         <p>Total: ${total}</p>
-        <button onclick = "eliminarDelCarrito(${id})" class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+        <button id="${id}" class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
         <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
         </svg></button>
         `
         contenedorCarrito.append(div);
         localStorage.setItem("carrito", JSON.stringify(carrito));
+        let boton = document.getElementById(id);
+        boton.addEventListener("click", (e) => {
+            eliminarDelCarrito(e, id);
+        })
     })
     contadorCarrito.innerText = carrito.length;
     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.total, 0);
 };
-const eliminarDelCarrito = (prodId) => {
+const eliminarDelCarrito = (e, prodId) => {
+    let botonclick = e.target;
+    botonclick.parentElement.remove()
     const item = carrito.find((prod) => prod.id === prodId);
     const indice = carrito.indexOf(item);
     carrito.splice(indice, 1);
     actualizarCarrito();
 };
+wapi.innerHTML= `
+<a href="http://wa.me/542617234395?text=Su pedido es: ${localStorage.getItem("carrito", JSON.stringify(carrito))}" target="_blank"><img class="footer__img" src="../resources/whatsapp.png" alt="logo whatsapp">
++54 2617234395</a>
+`;
--- js/productos.js
+++ js/productos.js
@@ -2,116 +2,75 @@ const vinosT = async () => {
     let response = await fetch("https://raw.githubusercontent.com/gaston964/JSON/main/ProductosBel.json");
     let data = await response.json();
     data.forEach(item => {
-        const {img,nombre,precio,id} = item;
+        const { img, nombre, precio, id } = item;
         let productos = document.createElement("div")
         productos.className = "card col-xs-12 col-md-6 col-lg-3 col-xl-3 my-3";
         productos.innerHTML = `
             <img src="${img}" alt="${nombre}" style="width:100%">
             <h1 class="text-center" >${nombre}</h1>
-            <p class="price">${precio}</p> 
+            <p class="price">$${precio}</p> 
             <h4>Unidad por Caja</h4>
             <p><button id="${id}" class="agregar-al-carro" >Agregar al carrito</button></p>
         `
-        contenedorTintos.append(productos);
+        fragmento1.append(productos);
     });
+    contenedorTintos.append(fragmento1)
 };
-const vinosB = async () =>{
+const vinosB = async () => {
     let response = await fetch("https://raw.githubusercontent.com/gaston964/JSON/main/ProductosBel2.json");
     let data = await response.json();
     data.forEach(item => {
-        const {img,nombre,precio,id} = item;
+        const { img, nombre, precio, id } = item;
         let productos = document.createElement("div")
         productos.className = "card col-xs-12 col-md-6 col-lg-3 col-xl-3 my-3";
         productos.innerHTML = `
             <img src="${img}" alt="${nombre}" style="width:100%">
             <h1 class="text-center" >${nombre}</h1>
-            <p class="price">${precio}</p> 
+            <p class="price">$${precio}</p> 
             <h4>Unidad por Caja</h4>
             <p><button id="${id}" class="agregar-al-carro" >Agregar al carrito</button></p>
         `
-        contenedorBlancos.append(productos);
+        fragmento2.append(productos);
     });
+    contenedorBlancos.append(fragmento2)
 };
-const vinosR = async () =>{
+const vinosR = async () => {
     let response = await fetch("https://raw.githubusercontent.com/gaston964/JSON/main/ProductosBel3.json");
     let data = await response.json();
     data.forEach(item => {
-        const {img,nombre,precio,id} = item;
+        const { img, nombre, precio, id } = item;
         let productos = document.createElement("div")
         productos.className = "card col-xs-12 col-md-6 col-lg-3 col-xl-3 my-3";
         productos.innerHTML = `
             <img src="${img}" alt="${nombre}" style="width:100%">
             <h1 class="text-center" >${nombre}</h1>
-            <p class="price">${precio}</p> 
+            <p class="price">$${precio}</p> 
             <h4>Unidad por Caja</h4>
             <p><button id="${id}" class="agregar-al-carro" >Agregar al carrito</button></p>
         `
-        contenedorRosados.append(productos);
+        fragmento3.append(productos);
     });
+    contenedorRosados.append(fragmento3);
 };
 const vinosE = async () => {
     let response = await fetch("https://raw.githubusercontent.com/gaston964/JSON/main/ProductosBel4.json");
     let data = await response.json();
     data.forEach(item => {
-        const {img,nombre,precio,id} = item;
+        const { img, nombre, precio, id } = item;
         let productos = document.createElement("div")
         productos.className = "card col-xs-12 col-md-6 col-lg-3 col-xl-3 my-3";
         productos.innerHTML = `
             <img src="${img}" alt="${nombre}" style="width:100%">
             <h1 class="text-center" >${nombre}</h1>
-            <p class="price">${precio}</p> 
+            <p class="price">$${precio}</p> 
             <h4>Unidad por Caja</h4>
             <p><button id="${id}" class="agregar-al-carro" >Agregar al carrito</button></p>
         `
-        contenedorEspumantes.append(productos);
+        fragmento4.append(productos);
     });
+    contenedorEspumantes.append(fragmento4);
 };
 vinosT();
 vinosB();
 vinosR();
 vinosE();
-
-
-
-
-
-/* const agregarAlCarrito = (prodId) => {
-    const existe = carrito.some(prod => prod.id === prodId)
-    if(existe){
-        const prod = carrito.map(prod => {
-            if(prod.id === prodId){
-                prod.cantidad++
-                prod.precio += prod.precio
-            }
-        })
-    }else{
-        let item = productos.find((prod) => prod.id === prodId);
-        carrito.push({
-            id: item.id,
-            nombre: item.nombre,
-            precio: item.precio,
-            cantidad : 1
-        });
-    }
-    actualizarCarrito();
-}; */
-
-/* const actualizarCarrito = () => {
-    contenedorCarrito.innerHTML = "";
-    carrito.forEach((prod) => {
-        let div = document.createElement("div");
-        div.className = "contenedor__carrito my-2";
-        div.innerHTML = `
-        <p>${prod.nombre}</p>
-        <p>$${prod.precio}</p>
-        <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
-        <button onclick = "eliminarDelCarrito(${prod.id})" class="btn btn-outline-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-        </svg></button>
-        `
-        contenedorCarrito.append(div);
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-    })
-    contadorCarrito.innerText = carrito.length;
-    precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0);
-} */
